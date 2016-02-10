6c64d1ce14c30fe04ae0b39847ff3c5e78c25518
tariq-aziz <tariq.aziz0547@gmail.com>
2016-02-10 10:35:02 -0500

    Update tabs.php

--- tabs.php
+++ tabs.php
@@ -7,62 +7,14 @@
 	
 	$tabChoice = $_GET['tab'];
 	
-	/*
-	if($tabChoice=="HighestRated"){
-
 		$tdCount=0;
-				
-        $query = "SELECT DISTINCT title FROM movies WHERE avg_rating>=60 ORDER BY `avg_rating` DESC";
-        $result = mysqli_query($connection, $query);
-				
-		echo "<table id='tabsContent'>";
-		echo"<tr>";
-		
-		//NEED TO USE .ini SINCE IMG URLS ARENT EXACTLY THE MOVIE TITLE
-		while($row = mysqli_fetch_array($result)){
-			
-			//First check if the row should end. If so, print a new row and reset count
-			if($tdCount>3){
-				echo "</tr> <tr>";
-				$tdCount=0;
-			}
-
-			//Print the given image, regardless of whether or not it's at a new row.
-			$title = $row['title'];
-		
-			$fileSearch = $title . ".image.url";
-			$imgSource = $ini_array["$fileSearch"];
-			echo "<td> <a href='movieDetailsPlan.php?movie=".$row['title']."'> 
-					<img src='$imgSource' alt='sorry'>";
-
-
-			$queryRating = "SELECT DISTINCT avg_rating FROM movies WHERE title='$title'";
-       		$resultRating = mysqli_query($connection, $queryRating);
-
-            //should only output once (only one avg per movie)
-           	while($ratingRow = mysqli_fetch_array($resultRating)){
-           		echo "<div class='posterRating'>" . $ratingRow['avg_rating']. "% </div> </a> </td>";
-
-       		}
-
-			$tdCount++;
-		
-		}
-
-		echo "</table>";
-
-	}
-
-	else{
-		*/
-		$tdCount=0;
-				
+		//obtain a list of movies from this genre, in order of descending rating
             	$query = "SELECT title, avg_rating FROM movies WHERE genre='$tabChoice' ORDER BY avg_rating DESC";
             	$result = mysqli_query($connection, $query);
-				
+				//create a table to display the movie mosters
 				echo "<table id='tabsContent'>";
 				echo"<tr>";
-				//NEED TO USE .ini SINCE IMG URLS ARENT EXACTLY THE MOVIE TITLE
+			
 				while($row = mysqli_fetch_array($result)){
 			
 					//First check if the row should end. If so, print a new row and reset count
@@ -81,19 +33,10 @@
 					echo "<td> <a href='movieDetailsPlan.php?movie=".$row['title']."'> 
 							<img class='posterImg' src='$imgSource' alt='$imgSource'>";
 
-
+					//obtain and display the average rating of the movie
 					$avgRating = $row['avg_rating'];
 					echo "<div class='posterRating'>" . $avgRating. "% </div> </a> </td>";
-					/*
-					//$queryRating = "SELECT DISTINCT avg_rating FROM movies WHERE title='$title'";
-            		//$resultRating = mysqli_query($connection, $queryRating);
-
-            		//should only output once (only one avg per movie)
-            		while($ratingRow = mysqli_fetch_array($resultRating)){
-            			echo "<div class='posterRating'>" . $avgRating. "% </div> </a> </td>";
-
-            		}
-					*/
+					
 					$tdCount++;
 		
 			}
