b743b96957ccf4a2dcb4f00e46f1f728350d3899
tariq-aziz <tariq.aziz0547@gmail.com>
2016-02-10 10:32:19 -0500

    Update rate.php

--- rate.php
+++ rate.php
@@ -17,39 +17,11 @@ $newQuery = "UPDATE users SET rating = '$rating' WHERE username='$id'
 			
 mysqli_query($connection, $newQuery);
 
-//MUST USE BACKTICKS for columns that have spaces (Hunger Games)!
-//$query = "UPDATE users SET `$movie` = '$rating' WHERE user_id='$id'";
-
-//mysqli_query($connection, $query);
-
-//MUST USE BACKTICKS for columns that have spaces!
-//$queryRatings = "SELECT * FROM users WHERE `$movie` IS NOT NULL";
 $queryAverage = "UPDATE movies SET avg_rating = (100*(SELECT AVG(rating) FROM users 
 				WHERE movie_title='$movie')/5) WHERE title='$movie'";
 
 $resultSet = mysqli_query($connection, $queryAverage);
 mysqli_query($resultSet);
 
-//$resultSet = mysqli_query($connection, $queryRatings);
-/*
-$total=0;
-$numRatings = mysqli_num_rows($resultSet);
-
-
-$average=0;
-$averageRounded=0;
-
-while($row = mysqli_fetch_array($resultSet)){
-	//use double quotes to include a variable in quotations (OR CAN JUST HAVE NO QUOTES)
-	//  --> DONT use single quotes! (those are for column names that you have; NOT VARIABLES)
-	$total+=$row["$movie"];
-}
-	
-$average = $total/$numRatings;
-$averageRounded = round($average, 1);
-
-$setAverage = "UPDATE movies SET average='$averageRounded' WHERE title='$movie'";
-mysqli_query($connection, $setAverage);
-*/
 
 ?>
